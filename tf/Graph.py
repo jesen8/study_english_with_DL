166d696a537c6df8dbe19f4cb0fb9e2e67ed0b9d
jesen <360697611@qq.com>
2019-01-29 08:52:48 +0800

    ADD

---
+++ tf/Graph.py
@@ -0,0 +1,74 @@
+# RIGHT
+def my_func(pred, tensor):
+  with tf.control_dependencies([pred]):
+    # The matmul op is created in the context, so a control dependency
+    # will be added.
+    return tf.matmul(tensor, tensor)
+
+
+
+tf.Graph.device(device_name_or_function)
+with g.device('/gpu:0'):
+  # All operations constructed in this context will be placed
+  # on GPU 0.
+  with g.device(None):
+    # All operations constructed in this context will have no
+    # assigned device.
+
+# Defines a function from `Operation` to device string.
+def matmul_on_gpu(n):
+  if n.type == "MatMul":
+    return "/gpu:0"
+  else:
+    return "/cpu:0"
+
+with g.device(matmul_on_gpu):
+  # All operations of type "MatMul" constructed in this context
+  # will be placed on GPU 0; all other operations will be placed
+  # on CPU 0.
+
+
+
+with tf.Graph().as_default() as g:
+  c = tf.constant(5.0, name="c")
+  assert c_1.name == "c"
+  c_1 = tf.constant(6.0, name="c")
+  assert c_1.name == "c_1"
+
+  # Creates a scope called "nested"
+  with g.name_scope("nested") as scope:
+    nested_c = tf.constant(10.0, name="c")
+    assert nested_c.name == "nested/c"
+
+    # Creates a nested scope called "inner".
+    with g.name_scope("inner"):
+      nested_inner_c = tf.constant(20.0, name="c")
+      assert nested_inner_c.name == "nested/inner/c"
+
+    # Create a nested scope called "inner_1".
+    with g.name_scope("inner"):
+      nested_inner_1_c = tf.constant(30.0, name="c")
+      assert nested_inner_1_c.name == "nested/inner_1/c"
+
+      # Treats `scope` as an absolute name scope, and
+      # switches to the "nested/" scope.
+      with g.name_scope(scope):
+        nested_d = tf.constant(40.0, name="d")
+        assert nested_d.name == "nested/d"
+
+        with g.name_scope(""):
+          e = tf.constant(50.0, name="e")
+          assert e.name == "e"
+
+
+
+@tf.RegisterGradient("CustomSquare")
+def _custom_square_grad(op, inputs):
+  # ...
+
+with tf.Graph().as_default() as g:
+  c = tf.constant(5.0)
+  s_1 = tf.square(c)  # Uses the default gradient for tf.square.
+  with g.gradient_override_map({"Square": "CustomSquare"}):
+    s_2 = tf.square(s_2)  # Uses _custom_square_grad to compute the
+                          # gradient of s_2.
